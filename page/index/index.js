289b12dfdc1205380cd655855cdb627148fb3854
ydc@qq.com <ydc@qq.com>
2017-09-20 17:18:20 +0800

    add list page

--- page/index/index.js
+++ page/index/index.js
@@ -9,18 +9,51 @@ Page({
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'],
+    
+    listData:[
+    {
+        imageUrl:'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        title:'科技改变生活科技改变生活科技改变生活科技改变生活科技改变生活科技改变生活科技改变生活科技改变生活科技改变生活科技改变生活科技改变生活',
+        time:'2017-2-12',
+    },
+    {
+      imageUrl: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      title: '科技改变生活',
+      time: '2017-2-12',
+    },
+    {
+      imageUrl: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      title: '科技改变生活',
+      time: '2017-2-12',
+    },
+    {
+      imageUrl: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      title: '科技改变生活',
+      time: '2017-2-12',
+    },
+    {
+      imageUrl: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      title: '科技改变生活',
+      time: '2017-2-12',
+    },
+   
+
+    ],
+    isHideLoadMore: false,
     indicatorDots: true,
     vertical: false,
     autoplay: true,
     interval: 2000,
-    duration: 1000
+    duration: 1000,
+    scrollTop: 100,
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
@@ -55,7 +88,14 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+    
+    //模拟加载
+    setTimeout(function () {
+      // complete
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+    }, 1500);
   },
 
   /**
@@ -63,6 +103,30 @@ Page({
    */
   onReachBottom: function () {
   
+    this.setData({
+      isHideLoadMore: true,
+    })
+    
+    setTimeout(() => {
+    
+      for(var i =0;i<3;i++){
+        this.data.listData.push({
+          
+          imageUrl: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          title: '科技改变生活'+i,
+          time: '2017-2-12',
+          
+        });
+      }
+      this.setData({
+       
+        listData:this.data.listData
+      })
+    }, 2000)
+
+    this.setData({
+      isHideLoadMore: false,
+    })
   },
 
   /**
